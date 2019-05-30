848e6c33dc9213deb89ccddfced84e77b1a991ef
huahaoworkspace@163.com <huahaoworkspace@163.com>
2019-05-30 09:25:22 +0800

    initial dev branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React,{Component} from 'react';
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
-// import createBrowserHistory from 'history/createBrowserHistory';
+import {BrowserRouter as Router,Route,Link,NavLink,Prompt} from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory';
 import {Provider} from 'react-redux';
 import {store} from './redux/index.js';
 import Home from './views/Home';
@@ -10,7 +10,7 @@ import Counter from './views/Counter'
 import './App.css';
 class App extends Component{
   render(){
-    // const customHistory = createBrowserHistory();
+    const customHistory = createBrowserHistory();
     return (
       <Provider store={store}>
         <Router>
@@ -19,13 +19,16 @@ class App extends Component{
               <ul>
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
-                <li><Link to="/topics">Topics</Link></li>
+                <li><Link to="/app">App</Link></li>
+                <li><NavLink to="/topics" activeClassName= "selected">Topics</NavLink></li>
               </ul>
             </nav>
             <Counter/>
+            <Prompt when={false} message = {location=>{const isApp = location.pathname.startsWith('/app');return isApp?`你确定要跳转到${location.pathname}吗`:true}}/>
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
             <Route path="/topics" component={Topics}/>
+            <Route path="/app" component={Home}/>
           </div>
         </Router>
       </Provider>
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,10 @@
 import React,{Component} from 'react';
 class Index extends Component{
+    componentDidMount(){
+        console.log("location is",this.props.location);
+        console.log("match is",this.props.match);
+        console.log("history is",this.props.history);
+    }
     render(){
         return (
             <h2>Home</h2>
